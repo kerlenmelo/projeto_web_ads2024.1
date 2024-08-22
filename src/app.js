3a10abc1784cb8b505356d5979552b6d08a40140
kerlenmelo <kerlen_1@hotmail.com>
2024-08-21 21:34:11 -0300

    Inicializando Servidor Node

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
 import express from "express";
+
+const app = express()
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+app.listen(3000, () => console.log("Servidor executando na porta 3000 ..."))
