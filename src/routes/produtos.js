12c5b567f8db61ae8a352c88111e12d728e64b05
Rafael Marques <rm180249@gmail.com>
2024-08-28 20:27:51 -0300

    Ajuste

---
+++ src/routes/produtos.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { getAllProducts } from '../models/productsModels.js';
+
+
+const routerProdutos = Router();
+
+
+routerProdutos.get("/api/produtos", async (_, response) => {
+    const produto = await getAllProducts()
+    return response.status(200).json(produto)
+});
+
+export { routerProdutos}
